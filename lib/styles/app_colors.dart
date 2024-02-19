9ba3512e7648cbc1c3162e1a61ef6d1a74d84609
Cao Nam <nam52871@donga.edu.vn>
2024-02-19 20:44:33 +0700

    Setup project

---
+++ lib/styles/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const white = (Color(0xffffffff));
+  static const black = (Color(0xffffffff));
+  static const primary = (Color(0xffffffff));
+}
